b9a23b3a6d233e301ef6082c8cbc8f7b0a8823d8
shivajaysaxena <144527966+shivajaysaxena@users.noreply.github.com>
2024-05-05 13:10:06 +0530

    Time: 4 ms (41.80%) | Memory: 13.4 MB (77.26%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        auto it = std::find(nums.begin(), nums.end(), target);
+        auto it = find(nums.begin(), nums.end(), target);
         if (it != nums.end()) {
             return it - nums.begin();
 
