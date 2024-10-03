1ef2c3bc0e562af18f256aaae9f6a7ddeb4f73be
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2024-10-03 11:10:12 +0530

    Time: 21 ms (39.84%) | Memory: 24.9 MB (61.38%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (mid % 2 == 1) {
-                mid--;
+        int n=nums.size();
+        int st = 0;
+        int end = n - 1;
+        if (n == 1) {
+            return nums[0];
+        }
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (mid == 0 && nums[0] != nums[1]) {
+                return nums[mid];
             }
-            if (nums[mid] != nums[mid + 1]) {
-                right = mid;
+            if (mid == n - 1 && nums[n - 1] != nums[1]) {
+                return nums[mid];
+            }
+            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            }
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid - 1]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
             } else {
-                left = mid + 2;
+                if (nums[mid] == nums[mid - 1]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
-        return nums[left];
+        return -1;
     }
 };
