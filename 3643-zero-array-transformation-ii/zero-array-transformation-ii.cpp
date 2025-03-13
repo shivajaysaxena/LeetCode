579a99939a7480698f11f38475b9db82650a716f
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-03-13 11:02:24 +0530

    Time: 15 ms (73.29%) | Memory: 323 MB (85.62%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size(), m = queries.size();
+        vector<int> freq(n + 1);
+        int op = 0, k = 0;
+        for (int i = 0; i < n; i++) {
+            for (; op < nums[i] - freq[i]; k++) {
+                if (k >= m)
+                    return -1;
+                int l = queries[k][0], r = queries[k][1], v = queries[k][2];
+                if (r < i)
+                    continue;
+                freq[max(l, i)] += v;
+                freq[r + 1] -= v;
+            }
+            op += freq[i];
+        }
+        return k;
+    }
+};
