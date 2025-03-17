382092e0a7b0d56a584fd1693d692e1f484028f2
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-03-17 11:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (8.99%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    int nCr(int n, int r) {
-        if (r > n)
-            return 0;
-        if (r == 0 || n == r)
+    int func(int i, int j, vector<vector<int>>& dp) {
+        // Base case
+        if (i == 0 && j == 0)
             return 1;
-        double res = 0;
-        for (int i = 0; i < r; i++) {
-            res += log(n - i) - log(i + 1);
-        }
-        return (int)round(exp(res));
+
+        /* If we go out of bounds or reach
+        a blocked cell, there are no ways.*/
+        if (i < 0 || j < 0)
+            return 0;
+
+        /* If we have already computed the number
+        of ways for this cell, return it.*/
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        /* Calculate the number of ways by
+        moving up and left recursively.*/
+        int up = func(i - 1, j, dp);
+        int left = func(i, j - 1, dp);
+
+        // Store the result in dp table and return it.
+        return dp[i][j] = up + left;
     }
 
+public:
+    /*Function to count the total ways
+    to reach (0,0) from (m-1,n-1)*/
     int uniquePaths(int m, int n) {
-        int t = (m - 1) + (n - 1);
+        /* Initialize a memoization table (dp) to
+        store the results of subproblems.*/
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        return nCr(t, m - 1);
+        // Return the total count(0 based indexing)
+        return func(m - 1, n - 1, dp);
     }
 };
