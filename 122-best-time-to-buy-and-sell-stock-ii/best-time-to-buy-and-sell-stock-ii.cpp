9fe53dad87843c8f9e78ecf426a61268e086b948
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-03-18 17:26:09 +0530

    Time: 9 ms (6.66%) | Memory: 22.2 MB (7.08%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,10 +1,44 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int ans = 0;
-        for (int i = 1; i < prices.size(); ++i) {
-            ans += max(prices[i] - prices[i - 1], 0);
+    int func(int ind, int buy, int n, vector<int>& arr,
+             vector<vector<int>>& dp) {
+        // Base case
+        if (ind == n) {
+            return 0;
         }
+        /* If the result for this state has
+        already been calculated, return it*/
+        if (dp[ind][buy] != -1) {
+            return dp[ind][buy];
+        }
+        int profit = 0;
+
+        // We can buy the stock
+        if (buy == 0) {
+            profit = max(0 + func(ind + 1, 0, n, arr, dp),
+                         (-1) * arr[ind] + func(ind + 1, 1, n, arr, dp));
+        }
+
+        // We can sell the stock
+        if (buy == 1) {
+            profit = max(0 + func(ind + 1, 1, n, arr, dp),
+                         arr[ind] + func(ind + 1, 0, n, arr, dp));
+        }
+
+        /* Store the value in dp array and
+        return the calculated profit */
+        return dp[ind][buy] = profit;
+    }
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        if (n == 0)
+            return 0;
+        // Initialize a DP table to memoize results
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+
+        int ans = func(0, 0, n, arr, dp);
+
+        // Return the maximum profit
         return ans;
     }
 };
