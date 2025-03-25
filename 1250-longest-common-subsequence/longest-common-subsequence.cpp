3987d61eb080f4917060fd93334cb25e1a9eaf6d
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-03-25 15:13:55 +0530

    Time: 46 ms (14.12%) | Memory: 27.5 MB (49.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,14 +1,33 @@
-class Solution {
-public:
-    int longestCommonSubsequence(string& a, string& b) {
-        short m[1001][1001] = {};
-        for (int i = 0; i < a.size(); ++i) {
-            for (int j = 0; j < b.size(); ++j) {
-                m[i + 1][j + 1] =
-                    a[i] == b[j] ? m[i][j] + 1 : max(m[i + 1][j], m[i][j + 1]);
-            }
-        }
+class Solution{
+private:
+    /* Function to find the length of the
+    Longest Common Subsequence (LCS)*/
+    int func(string& s1, string& s2, int ind1, int ind2, vector<vector<int>>& dp) {
+        // Base case
+        if (ind1 < 0 || ind2 < 0)
+            return 0;
 
-        return m[a.size()][b.size()];
+        /* If the result for this pair of indices
+        is already calculated, return it*/
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+
+        /* If the characters at the current 
+        indices match, increment the LCS length*/
+        if (s1[ind1] == s2[ind2])
+            return dp[ind1][ind2] = 1 + func(s1, s2, ind1 - 1, ind2 - 1, dp);
+        else
+            return dp[ind1][ind2] = max(func(s1, s2, ind1, ind2 - 1, dp), func(s1, s2, ind1 - 1, ind2, dp));
+    }
+public:
+    /* Function to calculate the length
+    of the Longest Common Subsequence*/
+    int longestCommonSubsequence(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        //Return the result
+        return func(str1, str2, n - 1, m - 1, dp);
     }
 };
