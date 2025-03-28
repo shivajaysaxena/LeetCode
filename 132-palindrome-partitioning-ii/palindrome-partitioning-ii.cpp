b2b94035a2aa164c63afbeede896f4415f09f583
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-03-28 23:40:12 +0530

    Time: 1529 ms (30.67%) | Memory: 9.4 MB (77.48%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    bool isPalindrome(int i, int j, string& s) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+public:
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int minCost = INT_MAX;
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(i, j, s)) {
+                    int cost = 1 + dp[j + 1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
+    }
+};
