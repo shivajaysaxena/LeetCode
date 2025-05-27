75d4b40c11570276b4e2b92a712ad5562772f74d
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-05-27 20:07:03 +0530

    Time: 159 ms (5.22%) | Memory: 54.1 MB (5.00%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-        vector<int> dist( n, INT_MAX );
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int K) {
+        vector<int> dist(n, INT_MAX);
         dist[src] = 0;
-        for( int i=0; i <= K; i++ ) {
-            vector<int> tmp( dist );
-            for( auto flight : flights ) {
-                if( dist[ flight[0] ] != INT_MAX ) {
-                    tmp[ flight[1] ] = min( tmp[flight[1]], dist[ flight[0] ] + flight[2] );
+        for (int i = 0; i <= K; i++) {
+            vector<int> tmp(dist);
+            for (auto flight : flights) {
+                if (dist[flight[0]] != INT_MAX) {
+                    tmp[flight[1]] =
+                        min(tmp[flight[1]], dist[flight[0]] + flight[2]);
                 }
             }
             dist = tmp;
