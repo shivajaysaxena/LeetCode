40ffd4f8a85379c05719e5d141b9a0ebcbc11c5c
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-06-01 17:58:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (20.99%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
         int n = intervals.size(), i = 0;
         int a = newInterval[0], b = newInterval[1];
         if (n == 0 || intervals[n - 1][1] < a) {
@@ -8,18 +9,18 @@ public:
             return intervals;
         }
         vector<vector<int>> ans;
-        while (i < n && intervals[i][1] < a){
+        while (i < n && intervals[i][1] < a) {
             ans.push_back(intervals[i++]);
         }
         a = min(a, intervals[i][0]);
-        while (i < n && intervals[i][0] <= b){ 
+        while (i < n && intervals[i][0] <= b) {
             b = max(b, intervals[i++][1]);
         }
         ans.push_back({a, b});
-        while (i < n){
+        while (i < n) {
             ans.push_back(intervals[i++]);
         }
-        
+
         return ans;
     }
 };
