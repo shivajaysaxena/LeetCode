304b98838a20c10229ca405ebdc6b42306b65fa9
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-06-09 11:29:24 +0530

    Time: 15 ms (9.14%) | Memory: 70.5 MB (6.36%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int binarySearch(vector<int> nums){
-        if(nums[nums.size() - 1] == 0)
+    int binarySearch(vector<int> nums) {
+        if (nums[nums.size() - 1] == 0)
             return nums.size();
         int low = 0;
         int high = nums.size() - 1;
         int idx;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(nums[mid] >= 1){
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] >= 1) {
                 idx = mid;
                 high = mid - 1;
-            }
-            else{
+            } else {
                 low = mid + 1;
             }
         }
-        return  idx;
+        return idx;
     }
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
         int maxCount = 0;
         int maxIndex = 0;
-        for(int i=0;i<mat.size();i++){
+        for (int i = 0; i < mat.size(); i++) {
             sort(mat[i].begin(), mat[i].end());
         }
-        for(int i=0;i<mat.size();i++){
-            int count = mat[0].size()- binarySearch(mat[i]);
-            if(count > maxCount){
+        for (int i = 0; i < mat.size(); i++) {
+            int count = mat[0].size() - binarySearch(mat[i]);
+            if (count > maxCount) {
                 maxCount = count;
                 maxIndex = i;
             }
