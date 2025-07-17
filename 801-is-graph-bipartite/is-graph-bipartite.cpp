622815f49038f8d1129211dc9445cefc8b3c0351
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-07-17 10:41:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (75.22%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -2,51 +2,26 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-
-        vector<int> L(n);
+        vector<int> color(n);
+        queue<int> q;
         for (int i = 0; i < n; i++) {
-            L[i] = -1;
-        }
-        vector<bool> vis(n, false);
-        queue<int> Q;
-
-        for (int o = 0; o < n; o++) {
-            if (vis[o] == false) {
-                Q.push(o);
-                while (!Q.empty()) {
-                    int u = Q.front();
-                    Q.pop();
-                    for (int v : graph[u]) {
-                        if (L[v] < 0) {
-                            Q.push(v);
-                            L[v] = L[u] + 1;
-                        }
+            if (color[i])
+                continue;
+            color[i] = 1;
+            for (q.push(i); !q.empty(); q.pop()) {
+                int cur = q.front();
+                for (int neighbor : graph[cur]) {
+                    if (!color[neighbor]) {
+                        color[neighbor] = -color[cur];
+                        q.push(neighbor);
                     }
-                }
-            }
-        }
-        bool ans = false;
 
-        int u, c;
-        bool brkchk = false;
-        for (u = 0; u < n; u++) {
-            c = 0;
-            for (int v : graph[u]) {
-                if (L[u] % 2 == L[v] % 2) {
-                    brkchk = true;
-                    break;
+                    else if (color[neighbor] == color[cur])
+                        return false;
                 }
-                if (brkchk == true) {
-                    break;
-                }
-                c++;
             }
         }
-        if (u == n && c == graph[n - 1].size()) {
-            ans = true;
-        } else {
-            ans = false;
-        }
-        return ans;
+
+        return true;
     }
 };
