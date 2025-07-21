c756d21a337d073d63902d03b31ace181ac18d64
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-07-21 13:24:54 +0530

    Time: 13 ms (18.07%) | Memory: 21.1 MB (47.31%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
+    unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj;
+    vector<string> result;
+
+    void dfs(string src) {
+        while (!adj[src].empty()) {
+            string next = adj[src].top();
+            adj[src].pop();
+            dfs(next);
+        }
+        result.push_back(src);
+    }
+
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string,
-                      priority_queue<string, vector<string>, greater<string>>>
-            graph;
-
-        for (const auto& ticket : tickets) {
-            graph[ticket[0]].push(ticket[1]);
+        for (auto& ticket : tickets) {
+            adj[ticket[0]].push(ticket[1]);
         }
-
-        vector<string> newItinerary;
-        stack<string> st;
-        st.push("JFK");
-
-        while (!st.empty()) {
-            string currentAirport = st.top();
-
-            if (graph.find(currentAirport) != graph.end() &&
-                !graph[currentAirport].empty()) {
-                st.push(graph[currentAirport].top());
-                graph[currentAirport].pop();
-            } else {
-                newItinerary.push_back(st.top());
-                st.pop();
-            }
-        }
-
-        reverse(newItinerary.begin(), newItinerary.end());
-        return newItinerary;
+        dfs("JFK");
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
