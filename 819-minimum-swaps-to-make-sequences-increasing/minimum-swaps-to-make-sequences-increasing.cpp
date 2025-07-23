01c087e68372ff1de5464fe295d017287f9fcbd7
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-07-23 17:13:34 +0530

    Time: 12 ms (75.35%) | Memory: 98.3 MB (74.17%) - LeetSync

---
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSwap(vector<int>& A, vector<int>& B) {
+        const size_t n = A.size();
+        vector<int> swap(n, n), no_swap(n, n);
+        swap[0] = 1;
+        no_swap[0] = 0;
+        for (size_t i = 1; i < n; ++i) {
+            if (A[i] > A[i - 1] && B[i] > B[i - 1]) {
+                swap[i] = swap[i - 1] + 1;
+                no_swap[i] = no_swap[i - 1];
+            }
+            
+            if (A[i] > B[i - 1] && B[i] > A[i - 1]) {
+                swap[i] = min(swap[i], no_swap[i - 1] + 1);
+                no_swap[i] = min(no_swap[i], swap[i - 1]);
+            }
+        }
+        
+        return min(swap.back(), no_swap.back());
+    }
+};
