13659aae17cbdf01782893c6d2347cb5f26c2d6f
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-08-05 18:05:09 +0530

    Time: 24 ms (46.26%) | Memory: 23.4 MB (64.44%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,19 +11,16 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root || root==p || root==q){
-            return root;
+        int small = min(p->val, q->val);
+        int large = max(p->val, q->val);
+        while (root != nullptr) {
+            if (root->val > large) // p, q belong to the left subtree
+                root = root->left;
+            else if (root->val < small) // p, q belong to the right subtree
+                root = root->right;
+            else // Now, small <= root.val <= large -> This root is the LCA between p and q
+                return root;
         }
-        TreeNode* left=lowestCommonAncestor(root->left,p,q);
-        TreeNode* right=lowestCommonAncestor(root->right,p,q);
-        if(left==NULL){
-            return right;
-        }
-        else if(right==NULL){
-            return left;
-        }
-        else{
-            return root;
-        }
+        return nullptr;
     }
 };
