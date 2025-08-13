7c45b9d4667433af6f2b14e5e4f40b755b3a26c9
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-08-13 18:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (46.41%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -4,9 +4,9 @@ public:
         if(n==0){
             return false;
         }
-        while(n%3 == 0){
-            n /= 3;
+        while(n%3==0){
+            n/=3;
         }
-        return n==1;  
+        return n==1;
     }
 };
