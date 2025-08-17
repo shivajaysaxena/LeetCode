8e633b5805ba1583aa696f620b8c0e73a178584c
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2025-08-17 11:15:17 +0530

    Time: 3 ms (61.18%) | Memory: 12.4 MB (96.00%) - LeetSync

---
+++ 867-new-21-game/new-21-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        // Edge cases
+        if (k == 0)
+            return 1.0;
+        if (n >= k - 1 + maxPts)
+            return 1.0;
+
+        vector<double> dp(n + 1, 0.0);
+        dp[0] = 1.0;
+
+        double window = 1.0;
+        double ans = 0.0;
+
+        for (int x = 1; x <= n; ++x) {
+            dp[x] = window / maxPts;
+
+            if (x < k) {
+                window += dp[x];
+            } else {
+                ans += dp[x];
+            }
+
+            if (x - maxPts >= 0) {
+                if (x - maxPts < k) {
+                    window -= dp[x - maxPts];
+                }
+            }
+        }
+        return ans;
+    }
+};
