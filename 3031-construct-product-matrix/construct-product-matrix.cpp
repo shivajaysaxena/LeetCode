bd3d28654124456fbc04780870ee7a46b1e0b81e
Shivajay Saxena <144527966+shivajaysaxena@users.noreply.github.com>
2026-03-24 23:06:05 +0530

    Time: 39 ms (51.66%) | Memory: 132.1 MB (71.96%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -1,32 +1,26 @@
-#define ll long long
 class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<ll>> pfx(m, vector<ll>(n + 1, 1));
-        vector<vector<ll>> sfx(m, vector<ll>(n + 1, 1));
+        const int MOD = 12345;
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> p(n, vector<int>(m));
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j)
-                pfx[i][j + 1] = (pfx[i][j] * grid[i][j]) % 12345;
-            for (int j = n - 1; j >= 0; --j)
-                sfx[i][j] = (sfx[i][j + 1] * grid[i][j]) % 12345;
-        }
-        vector<ll> pfxr(m + 1, 1), sfxr(m + 1, 1);
-        for (int i = 0, j = m - 1; i < m; ++i, --j) {
-            pfxr[i + 1] = (pfxr[i] * sfx[i][0]) % 12345;
-            sfxr[j] = (sfxr[j + 1] * sfx[j][0]) % 12345;
-        }
-
-        vector<vector<int>> ans(m, vector<int>(n, 1));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                ans[i][j] = (ans[i][j] * pfxr[i]) % 12345;
-                ans[i][j] = (ans[i][j] * sfxr[i + 1]) % 12345;
-                ans[i][j] = (ans[i][j] * pfx[i][j]) % 12345;
-                ans[i][j] = (ans[i][j] * sfx[i][j + 1]) % 12345;
+        long long suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                p[i][j] = suffix;
+                suffix = suffix * grid[i][j] % MOD;
             }
         }
-        return ans;
+
+        long long prefix = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                p[i][j] = p[i][j] * prefix % MOD;
+                prefix = prefix * grid[i][j] % MOD;
+            }
+        }
+
+        return p;
     }
 };
